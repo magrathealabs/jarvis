934c5946265523118c38ad4cd49474ef28273e89
schweigert <marlon.henry@magrathealabs.com>
2019-07-26 19:12:21 -0300

    Jarvis create a controller

--- jarvis.go
+++ jarvis.go
@@ -3,6 +3,5 @@ package main
 import "github.com/magrathealabs/jarvis/infrastructure/controllers"
 
 func main() {
-	err := controllers.NewAppController().Engine.Run()
-	panic(err)
+	controllers.NewAppController()
 }
