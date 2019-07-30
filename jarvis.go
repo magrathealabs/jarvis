bc3ca0091d77b60484d21f4e55e20b21e6ec21bd
schweigert <marlon.henry@magrathealabs.com>
2019-07-30 17:54:58 -0300

    Connect in bootstrap

--- jarvis.go
+++ jarvis.go
@@ -2,20 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/magrathealabs/jarvis/domain/repositories"
 	"github.com/magrathealabs/jarvis/infrastructure/datastore"
 	"github.com/magrathealabs/jarvis/infrastructure/handlers"
 	"github.com/magrathealabs/jarvis/libs/env"
 )
 
-func metricRepository() repositories.MetricRepository {
-	return datastore.NewMetricRepository(env.Graphite().Host(), env.Graphite().PortAsInt())
-}
-
 func engine() *gin.Engine {
 	engine := gin.Default()
 
-	handlers.NewRootHandler(metricRepository()).SetupRoutes(engine)
+	handlers.NewRootHandler(datastore.NewMetricRepositoryFromEnv()).SetupRoutes(engine)
 
 	return engine
 }
