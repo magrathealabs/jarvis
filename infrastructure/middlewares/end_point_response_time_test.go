ec8dd070cb196b581de674194343fff66138a87d
schweigert <marlon.henry@magrathealabs.com>
2019-07-31 13:48:59 -0300

    Test middlewares

--- infrastructure/middlewares/end_point_response_time_test.go
+++ infrastructure/middlewares/end_point_response_time_test.go
@@ -1,8 +1,12 @@
 package middlewares
 
 import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/magrathealabs/jarvis/infrastructure/datastore"
 
 	"github.com/krakenlab/gspec"
@@ -10,6 +14,21 @@ import (
 
 type EndpointRuntimeSuite struct {
 	gspec.Suite
+
+	Engine   *gin.Engine
+	Recorder *httptest.ResponseRecorder
+}
+
+func (suite *EndpointRuntimeSuite) SetupTest() {
+	suite.Engine = gin.Default()
+	suite.Recorder = httptest.NewRecorder()
+
+	repository := datastore.NewMetricRepositoryFromEnv()
+	suite.Engine.Use(EndpointRuntime(repository))
+
+	suite.Engine.GET("/", func(c *gin.Context) {
+		c.String(200, "Ok")
+	})
 }
 
 func (suite *EndpointRuntimeSuite) TestEndpointRuntime() {
@@ -17,6 +36,14 @@ func (suite *EndpointRuntimeSuite) TestEndpointRuntime() {
 	suite.NotNil(EndpointRuntime(repository))
 }
 
-func TestEndpointRuntimeSuite(t *testing.T) {
+func (suite *EndpointRuntimeSuite) TestGet() {
+	request, err := http.NewRequest(http.MethodGet, "/", strings.NewReader(""))
+	suite.NoError(err)
+
+	suite.Engine.ServeHTTP(suite.Recorder, request)
+	suite.Equal(http.StatusOK, suite.Recorder.Code)
+}
+
+func TestRootHandlerSuite(t *testing.T) {
 	gspec.Run(t, new(EndpointRuntimeSuite))
 }
