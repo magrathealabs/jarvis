6bb25a7014c61ab413666f9ce4bd465ae33b7352
schweigert <marlon.henry@magrathealabs.com>
2019-08-06 10:14:31 -0300

    Is now storing in domain

--- domain/services/mock_test.go
+++ domain/services/mock_test.go
@@ -1,8 +1,11 @@
 package services
 
+import "time"
+
 // MetricRepositoryMock struct
 type MetricRepositoryMock struct {
-	InsertMetricReturn error
+	InsertMetricReturn   error
+	InsertMetricAtReturn error
 }
 
 func NewMetricRepositoryMock() *MetricRepositoryMock {
@@ -10,5 +13,9 @@ func NewMetricRepositoryMock() *MetricRepositoryMock {
 }
 
 func (mock *MetricRepositoryMock) InsertMetric(tag string, value string) error {
+	return mock.InsertMetricReturn
+}
+
+func (mock *MetricRepositoryMock) InsertMetricAt(tag string, value string, at time.Time) error {
 	return mock.InsertMetricReturn
 }
--- domain/services/temperature_service.go
+++ domain/services/temperature_service.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"github.com/magrathealabs/jarvis/domain/models"
 	"github.com/magrathealabs/jarvis/domain/repositories"
 	"github.com/magrathealabs/jarvis/domain/services/forms"
+	"github.com/magrathealabs/jarvis/domain/services/responses"
 )
 
 // TemperatureService handles temperature metrics
@@ -16,6 +18,21 @@ func NewTemperatureService(metricRepository repositories.MetricRepository) *Temp
 }
 
 // StoresTemperature in metric repository
-func (service *TemperatureService) StoresTemperature(form *forms.StoresTemperatureForm) {
-	_ = form.BuildTemperature()
+func (service *TemperatureService) StoresTemperature(form *forms.StoresTemperatureForm) *responses.StoresTemperatureResponse {
+	response := responses.NewStoresTemperature()
+
+	response.Temperature = form.BuildTemperature()
+	if response.Temperature.Valid() {
+		response.Stored = service.insertTemperature(response.Temperature)
+	}
+
+	return response
+}
+
+func (service *TemperatureService) insertTemperature(temperature *models.Temperature) bool {
+	return service.MetricRepository.InsertMetricAt(
+		temperature.MetricTag(),
+		temperature.MetricValue(),
+		temperature.RecordedAt,
+	) == nil
 }
