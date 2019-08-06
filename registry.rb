e39e23f78f0d3dd7d6beccf7a06428c3dad15200
schweigert <marlon.henry@magrathealabs.com>
2019-08-06 13:47:43 -0300

    Registry in dockerhub

---
+++ registry.rb
@@ -0,0 +1,40 @@
+services = %w[
+    jarvis_jarvis
+]
+
+def now
+    @now ||= Time.now
+end
+
+def actual_dir
+    File.basename(Dir.getwd)
+end
+
+def git_project_prefix
+    "#{actual_dir}_"
+end
+
+def dockerhub_repository
+    "magrathealabs/jarvis"
+end
+
+def remove_prefix(service)
+    service.gsub(git_project_prefix, '')
+end
+
+def dated
+    [branch, now.year, now.month, now.day, now.hour, now.hour, now.min].join('.')
+end
+
+def branch
+    ENV['TRAVIS_BRANCH'] || 'unstable'
+end
+
+def tag(service)
+    "#{remove_prefix(service)}-#{dated}"
+end
+
+services.each do |service|
+   system("docker tag #{service} #{dockerhub_repository}:#{tag(service)}")
+   system("docker push #{dockerhub_repository}:#{tag(service)}")
+end
